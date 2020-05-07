c9ce0716bfd22ce0b9d7a6a6c6f8fd7ded1d1b96
Fatih Metin <cyberasker@gmail.com>
2020-05-07 19:54:31 +0300

    Some improvements, removing unused things (#7)
    * remove unused things
    
    * add more memory

--- src/test/scala/com/summarizer/services/SummaryServiceTest.scala
+++ src/test/scala/com/summarizer/services/SummaryServiceTest.scala
@@ -1,22 +1,17 @@
 package com.summarizer.services
 
-import com.google.inject.testing.fieldbinder.Bind
-import com.summarizer.repositories.SummaryRepository
 import org.scalatest.FunSpec
 import com.twitter.inject.Mockito
-import com.twitter.util.{Await, Future}
+import com.twitter.util.Await
 import org.scalatest.Matchers._
 
 class SummaryServiceTest extends FunSpec with CommonServices with Mockito {
 
   describe("Get summary") {
-    @Bind val summaryRepository = mock[SummaryRepository]
-    summaryRepository.findByContextOfText(anyString) returns Future(None)
-
     val lexicalChainService = new DefaultLexicalChainService
     val chainScoresService = new DefaultChainScoresService
     val extractSentenceService = new DefaultExtractSentenceService
-    val summaryService = new DefaultSummaryService(summaryRepository, preProcessService, nounService, lexicalChainService, chainScoresService, extractSentenceService, sentenceService)
+    val summaryService = new DefaultSummaryService(preProcessService, nounService, lexicalChainService, chainScoresService, extractSentenceService, sentenceService)
     val fileService = new DefaultFileService
     it("should create summary") {
       val fileResourcesPath = "text/1.txt"
